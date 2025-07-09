f36a50da6c4696151a178b6616b5cc2499748fa0
stirwin work local <stirwincastro@gmail.com>
2025-07-09 13:48:42 -0500

    posibles correciones restric mode off

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{
-        serverActions: true
-    }
+    reactStrictMode: true,
+   // experimental:{}
 };
 
 export default nextConfig;
