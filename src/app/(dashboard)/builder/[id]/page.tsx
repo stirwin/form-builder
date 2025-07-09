f95badf1a5f68b3f0a3af3ee1e4dbd7a23999b4e
stirwin work local <stirwincastro@gmail.com>
2025-07-09 09:43:53 -0500

    force dinamic en builder

--- src/app/(dashboard)/builder/[id]/page.tsx
+++ src/app/(dashboard)/builder/[id]/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { GetFormById } from '../../../../../actions/form';
 import Formbuilder from '@/components/form/disingner/Formbuilder';
 
+export const dynamic = 'force-dynamic';
 async function BuilderPage({params,}:{params:{id:string}}) {
     const {id}=params;
     const form = await GetFormById(Number(id));
