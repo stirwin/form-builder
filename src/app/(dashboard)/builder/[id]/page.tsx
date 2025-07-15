4d5946830cb933b5af8393dcbdfb7b3f7a8327fd
stirwin work local <stirwincastro@gmail.com>
2025-07-15 15:46:01 -0500

    despliegue para vercel

--- src/app/(dashboard)/builder/[id]/page.tsx
+++ src/app/(dashboard)/builder/[id]/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import React from 'react'
 import { GetFormById } from '../../../../../actions/form';
 import Formbuilder from '@/components/form/disingner/Formbuilder';
